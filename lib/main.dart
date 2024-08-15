da3a7d2cf77097d0ce4967cefa44d26422dd0f8b
Nandananandz <nandanasanal321@gmail.com>
2024-08-15 19:50:58 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:buspay_conductor/Screens/AuthenticationScreen/AuthenticationScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const buspay_conductor());
+}
+
+class buspay_conductor extends StatelessWidget {
+  const buspay_conductor({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder:(context, child) => MaterialApp(
+    home: Authenticationscreen() ),
+    );
+  }
+}
+
